6ceee58d95b0a1037b51acfe62273082e60a2f7c
Eriprando Pacces <eriprando.pacces@gmail.com>
2014-05-20 17:57:33 +0100

    adds support for trivial screen mirroring

--- s3.js
+++ s3.js
@@ -26,9 +26,6 @@ var allowCrossDomain = function(req, res, next) {
     next();
 }
 
-app.configure(function() {
-    app.use(allowCrossDomain);
-    app.use(express.static(__dirname));
-});
-
+app.use(allowCrossDomain);
+app.use(express.static(__dirname));
 app.listen(port);
